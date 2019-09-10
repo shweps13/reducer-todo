1cff11161d98bf0c544269aa75cd970b035963e2
Heorhii Siburov <g.siburov@gmail.com>
2019-09-10 12:12:37 -0700

    app ready for next work

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -6,19 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Reducer Todo App</h1>
       </header>
+      <div>
+        <p>App starts here</p>
+      </div>
     </div>
   );
 }
