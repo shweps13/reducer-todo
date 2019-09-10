0ff17823bee1525c4f8c85bad1a1c7042ac504f1
Heorhii Siburov <g.siburov@gmail.com>
2019-09-10 12:59:25 -0700

    process of rendering

--- reducer-todo/src/components/TodoCards.js
+++ reducer-todo/src/components/TodoCards.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
+
+import { initialState, todoReducer } from '../reducers/todoReducer';
+
+
+
 
 function TodoCards() {
+
+  // const [newTodo, setNewTodo] = useState();
+
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  console.log(state);
+
   return (
     <div className="TodoCards">
-        <p>Something important</p>
+        <div className="Card">
+          <h2>{state.item}</h2>
+            {state.completed == false &&
+          <h2>
+            Heyyo!
+          </h2>
+        }
+        </div>
     </div>
   );
 }
