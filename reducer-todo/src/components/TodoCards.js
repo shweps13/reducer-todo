6753bb6e9f402b29cbb2798006524f6d9a5f7449
Heorhii Siburov <g.siburov@gmail.com>
2019-09-10 13:14:56 -0700

    list of todos rendered

--- reducer-todo/src/components/TodoCards.js
+++ reducer-todo/src/components/TodoCards.js
@@ -1,4 +1,6 @@
 import React, { useState, useReducer } from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import { Icon } from 'semantic-ui-react'
 
 import { initialState, todoReducer } from '../reducers/todoReducer';
 
@@ -14,14 +16,15 @@ function TodoCards() {
 
   return (
     <div className="TodoCards">
-        <div className="Card">
-          <h2>{state.item}</h2>
-            {state.completed == false &&
-          <h2>
-            Heyyo!
-          </h2>
-        }
-        </div>
+      <div className="Main-Container">
+          {state.map((card) => (
+            <div className="Card" key={card.id}>
+              <h2>{card.item}</h2>
+              {card.completed === false && <Icon name='times rectangle' size='big' />}
+              {card.completed === true && <Icon name='check square outline' size='big' />}
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -1,8 +1,19 @@
-export const initialState = {
+export const initialState = [{
     item: 'Learn about reducers',
     completed: false,
     id: 3892987589
-  };
+  },
+  {
+    item: 'Feed the black cat',
+    completed: false,
+    id: 3892987590
+  },
+  {
+    item: 'Call to Elon Musk',
+    completed: true,
+    id: 3892987591
+  }
+];
   
   export const todoReducer = (state, action) => {
     switch (action.type) {
